bf524096293356a163ba3bfab9e82094e1868edb
OlimovN <olimonurmuhammad711@gmail.com>
2024-05-01 18:47:09 +0500

    7 - dars

---
+++ app.js
@@ -0,0 +1,107 @@
+
+
+
+// function elements(a) {
+//   let raqamKiriting = [];
+//   let son = 1;
+
+//   for (let i = 1; i <= a; i++) {
+//     if (i == 1) {
+//       raqamKiriting.push(i);
+//     } else {
+//       raqamKiriting.push((son += 2));
+//     }
+//   }
+
+//   return raqamKiriting;
+// }
+
+// const result = elements(7);
+
+// console.log(result);
+
+
+
+
+// let array = [5, 7, 9, 10, 6, 4];
+// let newArray = [];
+
+// function element(first) {
+//   first.forEach((tag) => {
+//     if (tag % 2 == 0) {
+//       newArr.unshift(tag);
+//     }
+//   });
+//   return newArr;
+// }
+
+// console.log(element(array));
+
+
+
+
+// let array = [5, 6, 7, 9, 4, 10];
+// let newArr = [];
+
+// function calc(kom) {
+//   for (let i = 0; i < kom.length; i++) {
+//     newArr.push(kom[i], arr[arr.length - 1 - i]);
+//   }
+//   console.log(newArr);
+// }
+// calc(array);
+
+
+
+
+// let numbers = [2, 4, 8, 6, 9, 12, 14];
+// let sum = 0;
+
+// function numRep(kom, a, b) {
+//   for (let i = 0; i < kom.length; i++) {
+//     if (i >= a && i <= b) {
+//       sum += kom[i];
+//     }
+//   }
+//   return sum;
+// }
+
+// console.log(numRep(arr, 2, 5));
+
+
+
+
+
+// let newArr = [];
+
+// let sonlar = [2, 8, 4, 9, 4, 7, 6, 8, 3, 1, 9];
+
+// function getKem(kom) {
+//   for (let i = 0; i < kom.length; i++) {
+//     if (!newArr.includes(ko [i])) {
+//       newArr.push(kom[i]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// console.log(getKem(kom));
+  
+
+
+
+// let kom = [5, 6, 8, 10, 12];
+// let newarray = [];
+
+// function kapChen(kom) {
+//   for (let i = 0; i < kom.length; i++) {
+//     if (i == array.length - 1) {
+//       newarray.push(kom[0]);
+//     } else {
+//       newarray.push(kom[i + 1]);
+//     }
+//   }
+//   return newArray;
+// }
+
+// console.log(kapChen(kom));
